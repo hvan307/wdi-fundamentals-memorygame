bba39f527e06433eb8fcdf485e7b814b4a5d0b1a
hvan307 <hongvan307@gmail.com>
2020-01-10 15:23:29 +0000

    created game logic

--- memory_game_HV/js/main.js
+++ memory_game_HV/js/main.js
@@ -1,8 +1,19 @@
 console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
 
+const cards = ["queen", "queen", "king", "king"];
+const cardInPlay = [];
+
+let cardOne = cards[0];
+cardInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+let cardTwo = cards[1];
+cardInPlay.push(cardTwo);
+console.log("user flipped "+ cardTwo);
+
+if (cardInPlay.length === 2 && cardOne === cardTwo) {
+	alert("You found a match!");
+} else {
+	(cardInPlay.length === 2 && cardOne !== cardTwo)
+	alert("Sorry, try again.");
+};
